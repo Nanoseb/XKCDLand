bc55c61d39236bc183f63a3af2ce7412db837f5f
Nanoseb <sebastien.lemaire@ecl13.ec-lyon.fr>
2017-11-18 12:46:56 +0000

    implement calculate new position

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ GAME_WINDOW_SIZE = (700, 500)
 BLACK = ( 0, 0, 0)
 WHITE = ( 255, 255, 255)
 
-def has_moved(keys):
-    """
-    return true if a key for movement was pressed
-    """
-    movement = (keys[pygame.K_LEFT] or
-                keys[pygame.K_RIGHT] or
-                keys[pygame.K_UP] or
-                keys[pygame.K_DOWN])
-    return movement
+# def has_moved(keys):
+#     """
+#     return true if a key for movement was pressed
+#     """
+#     movement = (keys[pygame.K_LEFT] or
+#                 keys[pygame.K_RIGHT] or
+#                 keys[pygame.K_UP] or
+#                 keys[pygame.K_DOWN])
+#     return movement
 
 def has_built(keys):
     """
@@ -34,7 +34,6 @@ def has_built(keys):
     built = (keys[pygame.K_b] or
              keys[pygame.K_u])
     return built
-
 
 
 
@@ -58,10 +57,10 @@ if __name__ == "__main__":
                     game_running = False
         
         keys = pygame.key.get_pressed()
-        if has_moved(keys):
-            # movement:
-            #   calculate next position
-            a_position = mp.calculate_next_position(keys, a_position)
+        # movement:
+        # calculate next position
+        a_position, has_moved = mp.calculate_next_position(keys, a_position, map_size)
+        if has_moved:
             #   check if space needs to be made visible in map_visible
             map_visible = mp.update_visible_map(a_position, map_visible)
             #   check if border was attacked in map_borders
