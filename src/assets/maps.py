dacad95bfc0e88d7fe9900c015d507d21f5e97e6
Alistair Lynn <arplynn@gmail.com>
2017-11-19 19:11:02 +0000

    Fix starting location

--- src/assets/maps.py
+++ src/assets/maps.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pygame
 
-start_position = (36, 39)
+start_position = (39, 36)
 map_background_img = pygame.image.load(os.path.abspath("src/assets/images/background_map.png"))
 x,y = map_background_img.get_rect().size
 cell_size = 25 # size in px of the grid cells squares
