4d6febb3fdfcd707f3d1d3320607da9345b8f4f4
Nanoseb <sebastien.lemaire@ecl13.ec-lyon.fr>
2017-11-19 18:33:49 +0000

    change map_cell name

--- src/assets/maps.py
+++ src/assets/maps.py
@@ -9,9 +9,9 @@ x,y = map_background_img.get_rect().size
 cell_size = 25 # size in px of the grid cells squares
 
 
-map_size = (int(y/cell_size), int(x/cell_size))
+map_cell_size = (int(y/cell_size), int(x/cell_size))
 
-map_visible = np.zeros((map_size[0], map_size[1]),)
+map_visible = np.zeros((map_cell_size[0], map_cell_size[1]),)
 
 
 map_borders = [1]
