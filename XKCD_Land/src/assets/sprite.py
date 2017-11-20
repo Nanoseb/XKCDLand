fa478b86445de90c6ebca31284eeaae238853082
Alistair Lynn <arplynn@gmail.com>
2017-11-20 01:12:35 +0000

    Fix up asset loading for this brave new world

--- XKCD_Land/src/assets/sprite.py
+++ XKCD_Land/src/assets/sprite.py
@@ -1,14 +1,13 @@
-import os
-from os.path import basename
+import os.path
 import pygame
 import glob
 image_dict = {}
 frame_speed = 0.3
 
-folder = "src/assets/images/"
-for image_file in glob.iglob(folder + '*.png'):
+folder = os.path.join(
+    os.path.dirname(__file__),
+    'images',
+)
+for image_file in glob.iglob(folder + '/*.png'):
     img_file = os.path.abspath(image_file)
-    image_dict[basename(image_file)] = pygame.image.load(img_file)
-
-
-
+    image_dict[os.path.basename(image_file)] = pygame.image.load(img_file)
