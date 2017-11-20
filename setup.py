caebc1c6ae986c4168fc370f63e276f556203dea
Alistair Lynn <arplynn@gmail.com>
2017-11-20 01:22:49 +0000

    Use `find_packages` from setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='XKCDland',
       version='0.0.1',
       description='A XKCD inspired game',
       license='MIT',
       url='https://github.com/Nanoseb/XKCDLand',
-      packages=['XKCD_Land'],
+      packages=find_packages(),
       scripts=['xkcdland'],
       install_requires=[
         "pygame",
